4177f9c64bae5c782080afbc703d34d9fdf6483e
Jeremy Van Patten <vanpattenjeremy@gmail.com>
2022-09-14 20:04:27 -0600

    Initial Project

---
+++ FPPP/Intermediate/Build/Win64/UnrealEditor/Inc/FPPP/FPPPGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FPPP_FPPPGameModeBase_generated_h
+#error "FPPPGameModeBase.generated.h already included, missing '#pragma once' in FPPPGameModeBase.h"
+#endif
+#define FPPP_FPPPGameModeBase_generated_h
+
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_SPARSE_DATA
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_RPC_WRAPPERS
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFPPPGameModeBase(); \
+	friend struct Z_Construct_UClass_AFPPPGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFPPPGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FPPP"), NO_API) \
+	DECLARE_SERIALIZER(AFPPPGameModeBase)
+
+
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAFPPPGameModeBase(); \
+	friend struct Z_Construct_UClass_AFPPPGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFPPPGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FPPP"), NO_API) \
+	DECLARE_SERIALIZER(AFPPPGameModeBase)
+
+
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFPPPGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFPPPGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFPPPGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFPPPGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFPPPGameModeBase(AFPPPGameModeBase&&); \
+	NO_API AFPPPGameModeBase(const AFPPPGameModeBase&); \
+public:
+
+
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFPPPGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFPPPGameModeBase(AFPPPGameModeBase&&); \
+	NO_API AFPPPGameModeBase(const AFPPPGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFPPPGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFPPPGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFPPPGameModeBase)
+
+
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_12_PROLOG
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_SPARSE_DATA \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_RPC_WRAPPERS \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_INCLASS \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_SPARSE_DATA \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_FPPP_Source_FPPP_FPPPGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FPPP_API UClass* StaticClass<class AFPPPGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_FPPP_Source_FPPP_FPPPGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
